71f6c091222caf8db0285805f623baebe649ce72
Nick Rodriguez <nrodrig1@gmail.com>
2023-12-13 11:20:20 +0100

    mod: apple_health_service using df to add data - minor update

--- apple_health_service.py
+++ apple_health_service.py
@@ -1,6 +1,6 @@
 import os
 import json
-from ws_models import sess, AppleHealthKit
+from ws_models import sess, engine, AppleHealthKit
 from ws_config import ConfigLocal, ConfigDev, ConfigProd
 from datetime import datetime
 from sqlalchemy.exc import IntegrityError
