e0daf2b2353950cd9bbc5a0fe74e42cafa655cb4
Nick Rodriguez <nrodrig1@gmail.com>
2023-12-13 11:16:29 +0100

    mod: apple_health_service using df to add data

--- apple_health_service.py
+++ apple_health_service.py
@@ -7,6 +7,7 @@ from sqlalchemy.exc import IntegrityError
 import logging
 from logging.handlers import RotatingFileHandler
 from sys import argv
+import pandas as pd
 
 match os.environ.get('FLASK_CONFIG_TYPE'):
     case 'dev':
@@ -42,55 +43,88 @@ def add_apple_health_to_database(user_id, apple_json_data_filename, check_all_bo
     logger_apple.info(f"- accessed add_apple_health_to_database for user_id: {user_id} -")
     print(f"- accessed add_apple_health_to_database for user_id: {user_id} -")
     user_id = int(user_id)
+
+    df_existing_user_data = get_existing_user_data(user_id)
+
     # ws_data_folder ="/Users/nick/Documents/_testData/_What_Sticks"
-    with open(os.path.join(config.APPLE_HEALTH_DIR, apple_json_data_filename), 'r') as file:
-        apple_json_data = json.load(file)
+    with open(os.path.join(config.APPLE_HEALTH_DIR, apple_json_data_filename), 'r') as new_user_data_path_and_filename:
+        # apple_json_data = json.load(new_user_data_path_and_filename)
+        df_new_user_data = pd.read_json(new_user_data_path_and_filename)
 
+    # Convert the 'value' column in both dataframes to string
+    df_new_user_data['value'] = df_new_user_data['value'].astype(str)
+    df_new_user_data['quantity'] = df_new_user_data['quantity'].astype(str)
+    # Perform the merge on specific columns
+    df_merged = pd.merge(df_new_user_data, df_existing_user_data, 
+                        on=['sampleType', 'startDate', 'endDate', 'UUID'], 
+                        how='outer', indicator=True)
+    # Filter out the rows that are only in df_new_user_data
+    df_unique_new_user_data = df_merged[df_merged['_merge'] == 'left_only']
+    # Drop columns ending with '_y'
+    df_unique_new_user_data = df_unique_new_user_data[df_unique_new_user_data.columns.drop(list(df_unique_new_user_data.filter(regex='_y')))]
+    # Filter out the rows that are duplicates (in both dataframes)
+    df_duplicates = df_merged[df_merged['_merge'] == 'both']
+    # Drop the merge indicator column from both dataframes
+    df_unique_new_user_data = df_unique_new_user_data.drop(columns=['_merge'])
+    df_duplicates = df_duplicates.drop(columns=['_merge'])
+    df_unique_new_user_data['user_id'] = user_id
+    # Convert 'user_id' from float to integer and then to string
+    df_unique_new_user_data['user_id'] = df_unique_new_user_data['user_id'].astype(int)
+    # Drop the 'metadataAppleHealth' and 'time_stamp_utc' columns
+    df_unique_new_user_data = df_unique_new_user_data.drop(columns=['metadataAppleHealth'])
+    # Fill missing values in 'time_stamp_utc' with the current UTC datetime
+    default_date = datetime.utcnow()
+    df_unique_new_user_data['time_stamp_utc'] = df_unique_new_user_data['time_stamp_utc'].fillna(default_date)
 
-    sorted_request_json = sorted(apple_json_data, key=lambda x: parse_date(x.get('startDate')), reverse=True)
-    count_of_added_records = 0
-    for i in range(0, len(sorted_request_json)):
-        # batch = sorted_request_json[i:i + batch_size]
-        try:
-            if add_entry_to_database(sorted_request_json[i], user_id):
-                count_of_added_records += 1
-                sess.commit()  # Commit the transaction for the individual entry
-            # logger_apple.info(f"- adding i: {count_of_added_records} -")
-        except IntegrityError as e:
-            sess.rollback()  # Rollback the transaction in case of an IntegrityError
-            logger_apple.info(f"IntegrityError encountered in batch: {e}")
-            if check_all_bool:
-                continue
-            else:
-                break
+    rename_dict = {}
+    rename_dict['metadata']='metadataAppleHealth'
+    rename_dict['sourceName_x']='sourceName'
+    rename_dict['value_x']='value'
+    rename_dict['device_x']='device'
+    rename_dict['sourceProductType_x']='sourceProductType'
+    rename_dict['sourceVersion_x']='sourceVersion'
+    rename_dict['quantity_x']='quantity'
+    df_unique_new_user_data.rename(columns=rename_dict, inplace=True)
+
+    added_rows = df_unique_new_user_data.to_sql('apple_health_kit', con=engine, if_exists='append', index=False)
+
+    # # sorted_request_json = sorted(apple_json_data, key=lambda x: parse_date(x.get('startDate')), reverse=True)
+    # count_of_added_records = 0
+    # for i in range(0, len(sorted_request_json)):
+    #     # batch = sorted_request_json[i:i + batch_size]
+    #     try:
+    #         if add_entry_to_database(sorted_request_json[i], user_id):
+    #             count_of_added_records += 1
+    #             sess.commit()  # Commit the transaction for the individual entry
+    #         # logger_apple.info(f"- adding i: {count_of_added_records} -")
+    #     except IntegrityError as e:
+    #         sess.rollback()  # Rollback the transaction in case of an IntegrityError
+    #         logger_apple.info(f"IntegrityError encountered in batch: {e}")
+    #         if check_all_bool:
+    #             continue
+    #         else:
+    #             break
     # print(f"- count_of_added_records: {count_of_added_records} -")
     count_of_user_apple_health_records = sess.query(AppleHealthKit).filter_by(user_id=user_id).count()
     logger_apple.info(f"- count of db records: {count_of_user_apple_health_records}")
     logger_apple.info(f"--- add_apple_health_to_database COMPLETE ---")
 
+def get_existing_user_data(user_id):
+    # user_id = 1
+    # Define the query
+    query = f"""
+    SELECT * 
+    FROM apple_health_kit 
+    WHERE user_id = {user_id};
+    """
+    # Execute the query and create a DataFrame
+    df_existing_user_data = pd.read_sql_query(query, engine)
 
-def add_entry_to_database(entry, user_id):
-    new_entry = AppleHealthKit(
-                user_id=user_id,
-                sampleType=entry.get('sampleType'),
-                startDate = entry.get('startDate'),
-                endDate = entry.get('endDate'),
-                metadataAppleHealth = entry.get('metadata'),
-                sourceName = entry.get('sourceName'),
-                sourceVersion = entry.get('sourceVersion'),
-                sourceProductType = entry.get('sourceProductType'),
-                device = entry.get('device'),
-                UUID = entry.get('UUID'),
-                quantity = entry.get('quantity'),
-                value = entry.get('value')
-    )
-    sess.add(new_entry)
-    sess.commit()
-    return True
 
-# Assuming your dates are in a format like '2023-11-11 10:35:46 +0000'
-def parse_date(date_str):
-    return datetime.strptime(date_str.split(' ')[0], '%Y-%m-%d')
+
+# # Assuming your dates are in a format like '2023-11-11 10:35:46 +0000'
+# def parse_date(date_str):
+#     return datetime.strptime(date_str.split(' ')[0], '%Y-%m-%d')
 
 def email_user(user_id, message, records_uploaded=0):
     headers = { 'Content-Type': 'application/json'}
