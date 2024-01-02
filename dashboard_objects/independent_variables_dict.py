39f0a900ba97098c12670d55d39b4617e44275c3
Nick Rodriguez <nrodrig1@gmail.com>
2024-01-02 18:33:14 +0100

    refactor: create_user_workouts_df missing arguments

--- dashboard_objects/independent_variables_dict.py
+++ dashboard_objects/independent_variables_dict.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from ws_analysis import create_user_qty_cat_df, corr_sleep_steps, corr_sleep_heart_rate, \
     create_user_workouts_df, corr_sleep_workouts
-
+from config_and_logger import config, logger_apple
 
 def user_correlations(user_id):
-    print("- in user_correlations ")
+    logger_apple.info("- in user_correlations ")
     df, list_of_user_data = create_user_qty_cat_df(user_id=user_id)
     list_of_arryIndepVarObjects_dict = []
     if 'HKCategoryTypeIdentifierSleepAnalysis' in list_of_user_data:
@@ -31,8 +31,11 @@ def user_correlations(user_id):
         arryIndepVarObjects_dict["noun"]= "daily average heart rate"
         list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
 
-    df_workouts, sampleTypeList = create_user_workouts_df
+    logger_apple.info("- in user_correlations ")
+    df_workouts, sampleTypeList = create_user_workouts_df(user_id=user_id)# <-- error
+
     if len(df_workouts) > 5:
+        logger_apple.info("- found more than 5 workouts -")
         arryIndepVarObjects_dict = {}
         # corr_sleep_heart_rate(df)
         arryIndepVarObjects_dict["independentVarName"]= "Avg Daily Workout Duration"
@@ -44,4 +47,8 @@ def user_correlations(user_id):
         arryIndepVarObjects_dict["noun"]= "avearge daily minutes worked out"
         list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
     
+    logger_apple.info("- list_of_arryIndepVarObjects_dict -")
+    logger_apple.info(list_of_arryIndepVarObjects_dict)
+    logger_apple.info("------------------------------------")
+
     return list_of_arryIndepVarObjects_dict
