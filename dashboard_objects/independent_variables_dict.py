ab1f332a19468802134999b6af347a129ad46399
Nick Rodriguez <nrodrig1@gmail.com>
2024-01-03 19:31:13 +0100

    refactor: added both dfs for corr_sleep_workouts

--- dashboard_objects/independent_variables_dict.py
+++ dashboard_objects/independent_variables_dict.py
@@ -5,50 +5,51 @@ from config_and_logger import config, logger_apple
 
 def user_correlations(user_id):
     logger_apple.info("- in user_correlations ")
-    df, list_of_user_data = create_user_qty_cat_df(user_id=user_id)
+    # df, list_of_user_data = create_user_qty_cat_df(user_id=user_id)
+    df_qty_cat, sampleTypeListQtyCat = create_user_qty_cat_df(user_id=user_id)
     list_of_arryIndepVarObjects_dict = []
-    if 'HKCategoryTypeIdentifierSleepAnalysis' in list_of_user_data:
+    if 'HKCategoryTypeIdentifierSleepAnalysis' in sampleTypeListQtyCat:
         arryIndepVarObjects_dict = {}
-        # corr_sleep_steps(df)
-        arryIndepVarObjects_dict["independentVarName"]= "Step Count"
-        arryIndepVarObjects_dict["forDepVarName"]= "Sleep Time"
-        correlation_value, obs_count = corr_sleep_steps(df)
-        arryIndepVarObjects_dict["correlationValue"]= correlation_value
-        arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
-        arryIndepVarObjects_dict["definition"]= "The count of your daily steps"
-        arryIndepVarObjects_dict["noun"]= "daily step count"
-        list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
+        # Steps
+        if 'HKQuantityTypeIdentifierStepCount' in sampleTypeListQtyCat:
+            arryIndepVarObjects_dict["independentVarName"]= "Step Count"
+            arryIndepVarObjects_dict["forDepVarName"]= "Sleep Time"
+            correlation_value, obs_count = corr_sleep_steps(df_qty_cat)
+            arryIndepVarObjects_dict["correlationValue"]= correlation_value
+            arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
+            arryIndepVarObjects_dict["definition"]= "The count of your daily steps"
+            arryIndepVarObjects_dict["noun"]= "daily step count"
+            list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
 
-    if 'HKQuantityTypeIdentifierHeartRate' in list_of_user_data:
-        arryIndepVarObjects_dict = {}
-        # corr_sleep_heart_rate(df)
-        arryIndepVarObjects_dict["independentVarName"]= "Heart Rate Avg"
-        arryIndepVarObjects_dict["forDepVarName"]= "Sleep Time"
-        correlation_value, obs_count = corr_sleep_heart_rate(df)
-        arryIndepVarObjects_dict["correlationValue"]= correlation_value
-        arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
-        arryIndepVarObjects_dict["definition"]= "The avearge of heart rates recoreded across all your devices"
-        arryIndepVarObjects_dict["noun"]= "daily average heart rate"
-        list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
+        # Heart Rate
+        if 'HKQuantityTypeIdentifierHeartRate' in sampleTypeListQtyCat:
+            arryIndepVarObjects_dict = {}
+            # corr_sleep_heart_rate(df)
+            arryIndepVarObjects_dict["independentVarName"]= "Heart Rate Avg"
+            arryIndepVarObjects_dict["forDepVarName"]= "Sleep Time"
+            correlation_value, obs_count = corr_sleep_heart_rate(df_qty_cat)
+            arryIndepVarObjects_dict["correlationValue"]= correlation_value
+            arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
+            arryIndepVarObjects_dict["definition"]= "The avearge of heart rates recoreded across all your devices"
+            arryIndepVarObjects_dict["noun"]= "daily average heart rate"
+            list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
 
-    logger_apple.info("- in user_correlations ")
-    df_workouts, sampleTypeList = create_user_workouts_df(user_id=user_id)# <-- error
+        logger_apple.info("- in user_correlations ")
+        df_workouts, sampleTypeListWorkouts = create_user_workouts_df(user_id=user_id)# <-- error
 
-    if len(df_workouts) > 5:
+        # Workouts 
         logger_apple.info("- found more than 5 workouts -")
         arryIndepVarObjects_dict = {}
-        # corr_sleep_heart_rate(df)
         arryIndepVarObjects_dict["independentVarName"]= "Avg Daily Workout Duration"
         arryIndepVarObjects_dict["forDepVarName"]= "Sleep Time"
-        correlation_value, obs_count = corr_sleep_workouts(df_workouts)
+        correlation_value, obs_count = corr_sleep_workouts(df_qty_cat, df_workouts)
         arryIndepVarObjects_dict["correlationValue"]= correlation_value
         arryIndepVarObjects_dict["correlationObservationCount"]= obs_count
         arryIndepVarObjects_dict["definition"]= "The avearge of daily duration recorded by all your devices and apps that share with Apple Health"
         arryIndepVarObjects_dict["noun"]= "avearge daily minutes worked out"
         list_of_arryIndepVarObjects_dict.append(arryIndepVarObjects_dict)
-    
-    logger_apple.info("- list_of_arryIndepVarObjects_dict -")
-    logger_apple.info(list_of_arryIndepVarObjects_dict)
-    logger_apple.info("------------------------------------")
+        logger_apple.info("- list_of_arryIndepVarObjects_dict -")
+        logger_apple.info(list_of_arryIndepVarObjects_dict)
+        logger_apple.info("------------------------------------")
 
     return list_of_arryIndepVarObjects_dict
